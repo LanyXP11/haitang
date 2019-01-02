8f79d2b5c83bd55ca209a34fc3f813beec7c5e3a
chenjiang <chenjiang@hetrone.com>
2019-01-02 20:42:18 +0800

    项目初始化

---
+++ src/main/java/com/haitang/HaitangApplication.java
@@ -0,0 +1,19 @@
+package com.haitang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/***
+ * <p>
+ *     项目启动
+ * </p>
+ */
+@SpringBootApplication
+public class HaitangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaitangApplication.class, args);
+    }
+
+}
+
